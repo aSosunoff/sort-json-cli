37b57fd7ca380d8d182abcf4d184cf95bef0bb4d
asosunoff <al.sosunoff@gmail.com>
2021-11-18 17:03:03 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,61 @@ import isObj from "lodash.isplainobject";
 
 const obj: Record<string, any> = {
   1: 1,
-  a: [1, 20, 2, 10, 3, 5, 6, 3, 55, 10, 30, 40],
-  b: ["1", "20", "2", "10", "3", "5", "6", "3", "55", "10", "30", "40"],
   20: "test",
+  /* a: [1, 20, 2, 10, 3, 5, 6, 3, 55, 10, 30, 40],
+  b: ["1", "20", "2", "10", "3", "5", "6", "3", "55", "10", "30", "40"],
   2: true,
-  10: null,
+  month: ["month", "months"],
+  numerals: {
+    "0": "0",
+    "1": "1",
+    "10": "10",
+    "11": "11",
+    "12": "12",
+    "13": "13",
+    "14": "14",
+    "15": "15",
+    "16": "16",
+    "17": "17",
+    "18": "18",
+    "19": "19",
+    "2": "2",
+    "20": "20",
+    "21": "21",
+    "22": "22",
+    "23": "23",
+    "24": "24",
+    "25": "25",
+    "26": "26",
+    "27": "27",
+    "28": "28",
+    "29": "29",
+    "3": "3",
+    "30": "30",
+    "31": "31",
+    "4": "4",
+    "5": "5",
+    "6": "6",
+    "7": "7",
+    "8": "8",
+    "9": "9",
+  },
+  10: {
+    list: [
+      {
+        desc: "First time using tabby simply link your card.",
+        title: "Set up your account",
+      },
+      {
+        desc: "Buy any number of times and spend up to {{limit}} {{currency}} during the month.",
+        title: "Check out with tabby",
+      },
+      {
+        desc: "Pay for all your purchases at the end of the month using any card.",
+        title: "Pay at the end of the month",
+      },
+    ],
+  },
   30: 2,
   40: 3,
   d: {
@@ -29,21 +79,69 @@ const obj: Record<string, any> = {
         n: "1",
       },
     },
-  },
+  }, */
 };
-
-const EOL = "\n";
-const SPACE = " ";
-const isStr = (something: any) => typeof something === "string";
-const getValue = (v: any) => (isStr(v) ? `"${v}"` : v);
-const getSortKeys = (obj: object) => Object.keys(obj).sort((a, b) => a.localeCompare(b));
 
 /* 
 fs
     .readFile(oneOfPaths, "utf8")
 */
 
-const json = (function work(obj, depth) {
+const getJsonStr = (obj: Record<string, any>) => {
+  const EOL = "\n";
+  const SPACE = " ";
+  const sRepeat = (depth: number) => SPACE.repeat(depth);
+  const isStr = (something: any) => typeof something === "string";
+  const isNumber = (something: any) => typeof something === "number";
+  const getValue = (v: any) => (isStr(v) ? `"${v}"` : v);
+  const getSortKeys = (obj: object) => Object.keys(obj).sort((a, b) => a.localeCompare(b));
+
+  const work = (obj: Record<string, any>, depth: number) => {
+    const keys = getSortKeys(obj);
+
+    let content = "";
+
+    for (const key of keys) {
+      if (isObj(obj[key])) {
+        content += `${EOL}${sRepeat(depth * 2)}"${key}": {${work(
+          obj[key],
+          depth + 1
+        )}${EOL}${sRepeat(depth * 2)}},`;
+      } else if (Array.isArray(obj[key]) && obj[key].length) {
+        let arraySort = "";
+
+        if (obj[key].every(isStr)) {
+          arraySort = `[${[...obj[key]]
+            .sort((a, b) => a.localeCompare(b))
+            .map((item) => `${EOL}${sRepeat((depth + 1) * 2)}"${item}"`)}${EOL}${sRepeat(
+            depth * 2
+          )}],`;
+        } else if (obj[key].every(isNumber)) {
+          arraySort = `[${[...obj[key]].sort((a, b) => a - b)}],`;
+        } else if (obj[key].every(isObj)) {
+          arraySort = `[${[...obj[key]].map(
+            (item) =>
+              `${EOL}${sRepeat((depth + 1) * 2)}{${work(item, depth + 2)}${EOL}${sRepeat(
+                (depth + 1) * 2
+              )}}`
+          )}${EOL}${sRepeat(depth * 2)}],`;
+        } else {
+          arraySort = obj[key];
+        }
+
+        content += `${EOL}${sRepeat(depth * 2)}"${key}": ${arraySort}`;
+      } else {
+        content += `${sRepeat(depth * 2)}"${key}": ${getValue(obj[key])},`;
+      }
+    }
+
+    return content;
+  };
+
+  return `{${EOL}${work(obj, 1)}${EOL}}`;
+};
+
+/* const json = (function work(obj, depth) {
   const keys = getSortKeys(obj);
 
   let content = "";
@@ -69,6 +167,6 @@ const json = (function work(obj, depth) {
   }
 
   return `{${content}${EOL}${SPACE.repeat((depth - 1) * 2)}}`;
-})(obj, 1);
+})(obj, 1); */
 
-console.log(json);
+console.log(getJsonStr(obj));
