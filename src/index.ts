1a70adf5837fc4bf38878d5bbaf03f9140739ed6
asosunoff <al.sosunoff@gmail.com>
2021-11-19 16:52:21 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import fs from "fs-extra";
-import isObj from "lodash.isplainobject";
 import chalk from "chalk";
 import path from "path";
-import jsStringEscape from "js-string-escape";
-import jsesc from "jsesc";
+import { getJsonStr } from "./get-json-str";
 
 const log = console.log;
 const prefix = "✨ sort-json-cli: ";
 
 const obj: Record<string, any> = {
-  onlyNeed: "You'll only need to do this once",
-  description: "Your privacy is very important to us. \nYour data is stored securely.",
+  /* onlyNeed: "You'll only need to do this once",
+  description: "Your privacy is very important to us. \nYour data is stored securely.", */
+  idConfirm: "1",
+  id_number: "ID number",
   /* 1: 1,
   20: "test",
   a: [1, 20, 2, 10, 3, 5, 6, 3, 55, 10, 30, 40],
@@ -104,89 +104,6 @@ const obj: Record<string, any> = {
       },
     },
   }, */
-};
-
-type OPTION = {
-  space?: string;
-  eol?: string;
-};
-
-type Required_v2<T extends object, K extends keyof T = keyof T> = Required<Pick<T, K>> & Omit<T, K>;
-
-const getJsonStr = (obj: Record<string, any>, option?: Partial<OPTION>) => {
-  const defaultOption = { space: " ", eol: "\n" };
-
-  const { eol, space }: Required_v2<OPTION, keyof typeof defaultOption> = {
-    ...defaultOption,
-    ...option,
-  };
-
-  const formatString = (v: string) => v.replace(/\n/g, "\\n").replace(/"/g, '\\"');
-  const sRepeat = (depth: number) => space.repeat(depth);
-  const isStr = (something: any): something is string => typeof something === "string";
-  const isNumber = (something: any) => typeof something === "number";
-  const getValue = <T>(v: T) => (isStr(v) ? `"${formatString(v)}"` : v);
-  const getSortKeys = (obj: object) =>
-    Object.keys(obj).sort((a, b) => {
-      const isUpperCaseA = /^[A-Z]+$/.test(a);
-      const isUpperCaseB = /^[A-Z]+$/.test(b);
-
-      if (isUpperCaseA && isUpperCaseB) {
-        return a.localeCompare(b);
-      } else if (isUpperCaseA) {
-        return -1;
-      } else if (isUpperCaseB) {
-        return 1;
-      }
-
-      return a.localeCompare(b);
-    });
-  const body = (content: string) => `{${eol}${content}${eol}}`;
-
-  const work = (obj: Record<string, any>, depth: number) => {
-    const keys = getSortKeys(obj);
-
-    const content: string[] = [];
-
-    for (const key of keys) {
-      if (isObj(obj[key])) {
-        const indent = sRepeat(depth * 2);
-
-        content.push(
-          `${indent}"${key}": {${eol}${work(obj[key], depth + 1).join(`,${eol}`)}${eol}${indent}}`
-        );
-      } else if (Array.isArray(obj[key]) && obj[key].length) {
-        let array = "";
-
-        if (obj[key].every(isStr)) {
-          array = `[${eol}${[...obj[key]]
-            .sort((a, b) => a.localeCompare(b))
-            .map((item) => `${sRepeat((depth + 1) * 2)}${getValue(item)}`)
-            .join(`,${eol}`)}${eol}${sRepeat(depth * 2)}]`;
-        } else if (obj[key].every(isNumber)) {
-          array = `[${[...obj[key]].sort((a, b) => a - b).join(",")}]`;
-        } else if (obj[key].every(isObj)) {
-          const indent = sRepeat((depth + 1) * 2);
-
-          array = `[${eol}${[...obj[key]]
-            .map(
-              (item) => `${indent}{${eol}${work(item, depth + 2).join(`,${eol}`)}${eol}${indent}}`
-            )
-            .join(`,${eol}`)}${eol}${sRepeat(depth * 2)}]`;
-        } else {
-          array = `[${obj[key].join(",")}]`;
-        }
-
-        content.push(`${sRepeat(depth * 2)}"${key}": ${array}`);
-      } else {
-        content.push(`${sRepeat(depth * 2)}"${key}": ${getValue(obj[key])}`);
-      }
-    }
-
-    return content;
-  };
-
-  return body(work(obj, 1).join(`,${eol}`));
 };
 
 const readFile = async (oneOfPaths: string) => {
