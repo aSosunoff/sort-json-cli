594710beb1f03515869a49709b2236488f730fae
asosunoff <al.sosunoff@gmail.com>
2021-11-19 18:00:35 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,110 +1,21 @@
 import fs from "fs-extra";
 import chalk from "chalk";
 import path from "path";
+
 import { getJsonStr } from "./get-json-str";
+import { cli } from "./cli";
 
 const log = console.log;
+
+if (cli.flags.version) {
+  log(cli.pkg.version);
+  process.exit(0);
+} else if (cli.flags.help) {
+  log(cli.help);
+  process.exit(0);
+}
+
 const prefix = "✨ sort-json-cli: ";
-
-const obj: Record<string, any> = {
-  /* onlyNeed: "You'll only need to do this once",
-  description: "Your privacy is very important to us. \nYour data is stored securely.", */
-  idConfirm: "1",
-  id_number: "ID number",
-  /* 1: 1,
-  20: "test",
-  a: [1, 20, 2, 10, 3, 5, 6, 3, 55, 10, 30, 40],
-  b: ["1", "20", "2", "10", "3", "5", "6", "3", "55", "10", "30", "40"],
-  111: [
-    {
-      desc: "First time using tabby simply link your card.",
-      title: "Set up your account",
-    },
-    {
-      desc: "Buy any number of times and spend up to {{limit}} {{currency}} during the month.",
-      title: "Check out with tabby",
-    },
-    {
-      desc: "Pay for all your purchases at the end of the month using any card.",
-      title: "Pay at the end of the month",
-    },
-  ],
-  10: {
-    list: [
-      {
-        desc: "First time using tabby simply link your card.",
-        title: "Set up your account",
-      },
-      {
-        desc: "Buy any number of times and spend up to {{limit}} {{currency}} during the month.",
-        title: "Check out with tabby",
-      },
-      {
-        desc: "Pay for all your purchases at the end of the month using any card.",
-        title: "Pay at the end of the month",
-      },
-    ],
-  },
-  2: true,
-  month: ["month", "months"],
-  numerals: {
-    "0": "0",
-    "1": "1",
-    "10": "10",
-    "11": "11",
-    "12": "12",
-    "13": "13",
-    "14": "14",
-    "15": "15",
-    "16": "16",
-    "17": "17",
-    "18": "18",
-    "19": "19",
-    "2": "2",
-    "20": "20",
-    "21": "21",
-    "22": "22",
-    "23": "23",
-    "24": "24",
-    "25": "25",
-    "26": "26",
-    "27": "27",
-    "28": "28",
-    "29": "29",
-    "3": "3",
-    "30": "30",
-    "31": "31",
-    "4": "4",
-    "5": "5",
-    "6": "6",
-    "7": "7",
-    "8": "8",
-    "9": "9",
-  },
-
-  30: 2,
-  40: 3,
-  d: {
-    n: "1",
-  },
-  d2: {
-    n: "1",
-    1: 1,
-    20: "test",
-    2: true,
-    10: null,
-    x: "2",
-  },
-  d3: {
-    d2: {
-      n: "1",
-      x: "2",
-      d: {
-        n: "1",
-      },
-    },
-  }, */
-};
 
 const readFile = async (oneOfPaths: string) => {
   const pathReadFile = path.resolve(__dirname, oneOfPaths);
@@ -130,4 +41,18 @@ const readFile = async (oneOfPaths: string) => {
 
 readFile("./source.json");
 
-/* console.log(getJsonStr(obj)); */
+/* console.log(
+  getJsonStr({
+    onlyNeed: "You'll only need to do this once",
+    a: [1, 20, 2, 10, 3, 5, 6, 3, 55, 10, 30, 40],
+    b: ["1", "20", "2", "10", "3", "5", "6", "3", "55", "10", "30", "40"],
+    d2: {
+      n: "1",
+      1: 1,
+      20: "test",
+      2: true,
+      10: null,
+      x: "2",
+    },
+  })
+); */
