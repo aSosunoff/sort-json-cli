f9d0adb6a5735f8f3d6536479f4ae1171d17d85c
asosunoff <al.sosunoff@gmail.com>
2021-11-21 12:29:37 +0300

    fix

--- src/test.ts
+++ src/test.ts
@@ -2,121 +2,27 @@ import fs from "fs-extra";
 import isObj from "lodash.isplainobject";
 import chalk from "chalk";
 import path from "path";
-import jsStringEscape from "js-string-escape";
-import jsesc from "jsesc";
 
 const log = console.log;
 const prefix = "✨ sort-json-cli: ";
 
 const obj: Record<string, any> = {
   /* onlyNeed: "You'll only need to do this once", */
-  a: {
-    x: 1,
-    y: 2,
-    z: {
-      x: 1,
-      y: 2,
-      z: {
-        x: 1,
-        y: 2,
-      },
+  a1: 1,
+  b1: {
+    xb2: 1,
+    yb2: 1,
+  },
+  x1: 1,
+  y1: {
+    a2: 1,
+    x2: 1,
+    y2: {
+      a3: 1,
+      x3: 1,
+      y3: 1,
     },
   },
-
-  /* description: "Your privacy is very important to us. \nYour data is stored securely.",
-  1: 1,
-  20: "test", */
-  /* a: [1, 20, 2, 10, 3, 5, 6, 3, 55, 10, 30, 40], */
-  /* b: ["1", "20", "2", "10", "3", "5", "6", "3", "55", "10", "30", "40"], */
-  /* 111: [
-    {
-      desc: "First time using tabby simply link your card.",
-      title: "Set up your account",
-    },
-    {
-      desc: "Buy any number of times and spend up to {{limit}} {{currency}} during the month.",
-      title: "Check out with tabby",
-    },
-    {
-      desc: "Pay for all your purchases at the end of the month using any card.",
-      title: "Pay at the end of the month",
-    },
-  ],
-  10: {
-    list: [
-      {
-        desc: "First time using tabby simply link your card.",
-        title: "Set up your account",
-      },
-      {
-        desc: "Buy any number of times and spend up to {{limit}} {{currency}} during the month.",
-        title: "Check out with tabby",
-      },
-      {
-        desc: "Pay for all your purchases at the end of the month using any card.",
-        title: "Pay at the end of the month",
-      },
-    ],
-  },
-  2: true,
-  month: ["month", "months"],
-  numerals: {
-    "0": "0",
-    "1": "1",
-    "10": "10",
-    "11": "11",
-    "12": "12",
-    "13": "13",
-    "14": "14",
-    "15": "15",
-    "16": "16",
-    "17": "17",
-    "18": "18",
-    "19": "19",
-    "2": "2",
-    "20": "20",
-    "21": "21",
-    "22": "22",
-    "23": "23",
-    "24": "24",
-    "25": "25",
-    "26": "26",
-    "27": "27",
-    "28": "28",
-    "29": "29",
-    "3": "3",
-    "30": "30",
-    "31": "31",
-    "4": "4",
-    "5": "5",
-    "6": "6",
-    "7": "7",
-    "8": "8",
-    "9": "9",
-  },
-
-  30: 2,
-  40: 3,
-  d: {
-    n: "1",
-  },
-  d2: {
-    n: "1",
-    1: 1,
-    20: "test",
-    2: true,
-    10: null,
-    x: "2",
-  },
-  d3: {
-    d2: {
-      n: "1",
-      x: "2",
-      d: {
-        n: "1",
-      },
-    },
-  }, */
 };
 
 type OPTION = {
@@ -124,7 +30,10 @@ type OPTION = {
   eol?: string;
 };
 
-type Required_v2<T extends object, K extends keyof T = keyof T> = Required<Pick<T, K>> & Omit<T, K>;
+type Required_v2<T extends object, K extends keyof T = keyof T> = Required<
+  Pick<T, K>
+> &
+  Omit<T, K>;
 
 const getJsonStr = (obj: Record<string, any>, option?: Partial<OPTION>) => {
   const defaultOption = { space: " ", eol: "\n" };
@@ -134,29 +43,17 @@ const getJsonStr = (obj: Record<string, any>, option?: Partial<OPTION>) => {
     ...option,
   };
 
-  const formatString = (v: string) => v.replace(/\n/g, "\\n").replace(/"/g, '\\"');
+  const formatString = (v: string) =>
+    v.replace(/\n/g, "\\n").replace(/"/g, '\\"');
   const sRepeat = (depth: number) => space.repeat(depth);
-  const isStr = (something: any): something is string => typeof something === "string";
+  const isStr = (something: any): something is string =>
+    typeof something === "string";
   const isNumber = (something: any) => typeof something === "number";
   const getValue = <T>(v: T) => (isStr(v) ? `"${formatString(v)}"` : v);
   const getSortKeys = (obj: object) =>
-    Object.keys(obj).sort((a, b) => {
-      const isUpperCaseA = /^[A-Z]+$/.test(a);
-      const isUpperCaseB = /^[A-Z]+$/.test(b);
+    Object.keys(obj).sort((a, b) => (a > b ? 1 : -1));
 
-      if (isUpperCaseA && isUpperCaseB) {
-        return a.localeCompare(b);
-      } else if (isUpperCaseA) {
-        return -1;
-      } else if (isUpperCaseB) {
-        return 1;
-      }
-
-      return a.localeCompare(b);
-    });
-  const body = (content: string) => `{${eol}${content}${eol}}`;
-
-  /*  */
+  //#region QueueContext
   interface QueueContext {
     current: number;
     keys: string[];
@@ -194,8 +91,9 @@ const getJsonStr = (obj: Record<string, any>, option?: Partial<OPTION>) => {
       });
     }
   }
+  //#endregion
 
-  /*  */
+  //#region Handler
   type HandlerFn = (content: string) => string;
 
   class Handler {
@@ -205,12 +103,50 @@ const getJsonStr = (obj: Record<string, any>, option?: Partial<OPTION>) => {
       this.handlers.push(handler);
     }
 
+    getLastHandler() {
+      return this.handlers.pop();
+    }
+
     reset() {
       this.handlers.length = 0;
     }
   }
+  //#endregion
 
-  /*  */
+  //#region Context
+  class Context {
+    content: string[] = [];
+    subContent: string[] = [];
+    saveContent: Array<string[]> = [];
+
+    add(v: string) {
+      this.content.push(v);
+    }
+
+    saveContext() {
+      this.saveContent.push([...this.subContent]);
+      this.subContent.length = 0;
+    }
+
+    addToSub(v: string) {
+      this.subContent.push(v);
+    }
+
+    addToSubWithSave(v: string) {
+      const saveContextLast = this.saveContent.pop();
+
+      if (saveContextLast) {
+        this.subContent.push(...[...saveContextLast, v]);
+      } else {
+        this.subContent.push(v);
+      }
+    }
+
+    clearSub() {
+      this.subContent.length = 0;
+    }
+  }
+  //#endregion
 
   const compose =
     (...fn: Array<(...arg: any) => any>) =>
@@ -220,20 +156,20 @@ const getJsonStr = (obj: Record<string, any>, option?: Partial<OPTION>) => {
   const work = (obj: Record<string, any>) => {
     const queue = new Queue();
     const handler = new Handler();
+    const ctx = new Context();
 
     queue.addItemQueue(obj, 1);
-
-    const content: string[] = [];
-    const _content: string[] = [];
 
     while (queue.items.length) {
       const item = queue.items[queue.items.length - 1];
 
-      let isRemoveQueueItems = true;
+      let isCommit = true;
 
       for (const { key, data, depth } of item) {
         if (isObj(data)) {
           queue.addItemQueue(data, depth + 1);
+
+          ctx.saveContext();
 
           handler.addHandler((content) => {
             const indent = sRepeat(depth * 2);
@@ -241,18 +177,21 @@ const getJsonStr = (obj: Record<string, any>, option?: Partial<OPTION>) => {
             return `${indent}"${key}": {${eol}${content}${eol}${indent}}`;
           });
 
-          isRemoveQueueItems = false;
+          isCommit = false;
+
           break;
-        } else if (Array.isArray(data) && data.length) {
+        }
+
+        if (Array.isArray(data) && data.length) {
           let array = "";
 
           if (data.every(isStr)) {
-            array = `[${eol}${[...data]
+            /* array = `[${eol}${[...data]
               .sort((a, b) => a.localeCompare(b))
               .map((item) => `${sRepeat((depth + 1) * 2)}${getValue(item)}`)
-              .join(`,${eol}`)}${eol}${sRepeat(depth * 2)}]`;
+              .join(`,${eol}`)}${eol}${sRepeat(depth * 2)}]`; */
           } else if (data.every(isNumber)) {
-            array = `[${[...data].sort((a, b) => a - b).join(",")}]`;
+            /* array = `[${[...data].sort((a, b) => a - b).join(",")}]`; */
           } else if (data.every(isObj)) {
             /* const indent = sRepeat((depth + 1) * 2);
 
@@ -262,35 +201,41 @@ const getJsonStr = (obj: Record<string, any>, option?: Partial<OPTION>) => {
               )
               .join(`,${eol}`)}${eol}${sRepeat(depth * 2)}]`; */
           } else {
-            array = `[${data.join(",")}]`;
+            /* array = `[${data.join(",")}]`; */
           }
 
-          _content.push(`${sRepeat(depth * 2)}"${key}": ${array}`);
+          /* innerContent.push(`${sRepeat(depth * 2)}"${key}": ${array}`); */
         } else {
-          /* queue.addHandler(() => `${sRepeat(depth * 2)}"${key}": ${getValue(data)}`); */
-          _content.push(`${sRepeat(depth * 2)}"${key}": ${getValue(data)}`);
+          ctx.addToSub(`${sRepeat(depth * 2)}"${key}": ${getValue(data)}`);
         }
       }
 
-      if (isRemoveQueueItems) {
-        const contentResult = compose(
-          (content) => content,
-          ...handler.handlers
-        )(_content.join(`,${eol}`));
+      if (isCommit) {
+        const lastHandler = handler.getLastHandler();
 
-        _content.length = 0;
-        handler.reset();
+        if (lastHandler) {
+          if (ctx.saveContent.length) {
+            const data = lastHandler(ctx.subContent.join(`,${eol}`));
+            ctx.clearSub();
+            ctx.addToSubWithSave(data);
+          }
+        } else {
+          const contentResult = ctx.subContent.join(`,${eol}`);
+          ctx.clearSub();
 
-        content.push(contentResult);
+          if (contentResult) {
+            ctx.add(contentResult);
+          }
+        }
 
         queue.items.pop();
       }
     }
 
-    return content;
+    return `{${eol}${ctx.content.join(`,${eol}`)}${eol}}`;
   };
 
-  return body(work(obj).join(`,${eol}`));
+  return work(obj);
 };
 
 const readFile = async (oneOfPaths: string) => {
