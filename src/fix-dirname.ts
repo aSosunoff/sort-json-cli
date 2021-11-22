1b4ab829cfb7c20278d6215d210d2d15206db563
asosunoff <al.sosunoff@gmail.com>
2021-11-22 13:01:56 +0300

    fix

---
+++ src/fix-dirname.ts
@@ -0,0 +1,9 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export { __dirname };
+
+/* https://ru.hexlet.io/blog/posts/chto-takoe-__dirname-v-javascript */
