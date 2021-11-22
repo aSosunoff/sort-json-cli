2bad4c894d64ffd94b94eb023beface97b0881fd
asosunoff <al.sosunoff@gmail.com>
2021-11-22 13:02:04 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
 import fs from "fs-extra";
 import chalk from "chalk";
 import path from "path";
+import { __dirname } from "./fix-dirname.js";
 
-import { getJsonStr } from "./get-json-str";
-/* import { cli } from "./cli"; */
+import { getJsonStr } from "./get-json-str.js";
+import { cli } from "./cli.js";
 
 const log = console.log;
 
-/* if (cli.flags.version) {
+if (cli.flags.version) {
   log(cli.pkg.version);
   process.exit(0);
 } else if (cli.flags.help) {
   log(cli.help);
   process.exit(0);
-} */
+}
 
 const prefix = "✨ sort-json-cli: ";
 
@@ -40,19 +41,3 @@ const readFile = async (oneOfPaths: string) => {
 };
 
 readFile("./source.json");
-
-/* console.log(
-  getJsonStr({
-    onlyNeed: "You'll only need to do this once",
-    a: [1, 20, 2, 10, 3, 5, 6, 3, 55, 10, 30, 40],
-    b: ["1", "20", "2", "10", "3", "5", "6", "3", "55", "10", "30", "40"],
-    d2: {
-      n: "1",
-      1: 1,
-      20: "test",
-      2: true,
-      10: null,
-      x: "2",
-    },
-  })
-); */
