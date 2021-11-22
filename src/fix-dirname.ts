b9e45ab329e4149614c5edddbad39bb928435eb0
asosunoff <al.sosunoff@gmail.com>
2021-11-22 14:15:50 +0300

    delete

--- src/fix-dirname.ts
+++
@@ -1,9 +0,0 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export { __dirname };
-
-/* https://ru.hexlet.io/blog/posts/chto-takoe-__dirname-v-javascript */
--- src/helper-types.ts
+++
@@ -1,2 +0,0 @@
-export type Required_v2<T extends object, K extends keyof T = keyof T> = Required<Pick<T, K>> &
-  Omit<T, K>;
