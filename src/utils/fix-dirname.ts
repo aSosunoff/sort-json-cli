7434cef9b5843fc0e79817cdb810b3e055d6caf0
asosunoff <al.sosunoff@gmail.com>
2021-11-22 14:16:34 +0300

    fix

--- src/utils/fix-dirname.ts
+++ src/utils/fix-dirname.ts
@@ -2,9 +2,6 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 export const getDirname = (impoertMeta: ImportMeta) => {
-  /* const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename); */
-
   const __filename = fileURLToPath(impoertMeta.url);
   const __dirname = path.dirname(__filename);
 
