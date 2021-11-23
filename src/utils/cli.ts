9eb51e95c10244ad65f10e0e73855202ef87bd45
asosunoff <al.sosunoff@gmail.com>
2021-11-23 22:28:00 +0300

    fix

--- src/utils/cli.ts
+++ src/utils/cli.ts
@@ -9,28 +9,36 @@ export const cli = meow(
     or, just type "jsonsort" and it will let you pick a file.
   
     Options
+      -n, --nodemodules      Don't ignore any node_modules folders
       -t, --tabs             Use tabs for JSON file indentation
       -i, --indentationCount How many spaces or tabs to use (default = 2 spaces or 1 tab)
       -h, --help             Shows this help
       -v, --version          Shows the current version
       -c, --ci               Only exits with non-zero code if files COULD BE sorted
       -a, --arrays           Also sort any arrays if they contain only string elements
+      -p, --pack             Exclude all package.json files
+      -ta, --target          Save to target file
   
     Example
       Call anywhere using glob patterns. If you put them as string, this library
-      will parse globs. If you put as system globs without quotes, your shell will expand them.
+      will parse globs. If you put as system globs 2ee22e2ewithout quotes, your shell will expand them.
   `,
   {
     importMeta: import.meta,
     flags: {
-      /* nodemodules: {
-          type: "boolean",
-          alias: "n",
-          default: false,
-        }, */
+      nodemodules: {
+        type: "boolean",
+        alias: "n",
+        default: false,
+      },
       tabs: {
         type: "boolean",
         alias: "t",
+        default: false,
+      },
+      target: {
+        type: "boolean",
+        alias: "r",
         default: false,
       },
       /* silent: {
@@ -43,12 +51,12 @@ export const cli = meow(
         alias: "a",
         default: false,
       },
-      /* pack: {
+      pack: {
         type: "boolean",
         alias: "p",
         default: false,
       },
-      dry: {
+      /* dry: {
         type: "boolean",
         alias: "d",
         default: false,
