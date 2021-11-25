8797b318cf7d0425cf1827d5c8d067286b4da0ff
asosunoff <al.sosunoff@gmail.com>
2021-11-25 18:27:28 +0300

    added test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,17 +1,64 @@
-import { exec } from "child_process";
+import path from "path";
+import util from "util";
+import childProcess from "child_process";
+import fs from "fs";
+import fsx from "fs-extra";
+
+const exec = util.promisify(childProcess.exec);
 
 describe("Check CLI sortjsoncli", () => {
-  it("should by contain output line", (done) => {
-    exec("node --loader ts-node/esm ./src/index.ts ./src/source.json -d", (error, stdout) => {
-      if (error) {
-        done(error);
-      }
+  const testFileName = "source.json";
+  const testFileTargetName = "source.target.json";
 
-      expect(stdout).toBe(
-        "✨ sort-json-cli: We'd try to sort the following files:\n./src/source.json\n"
-      );
+  it("should by contain output line", async () => {
+    await fs.promises.writeFile(
+      path.resolve(__dirname, testFileName),
+      JSON.stringify(
+        {
+          1: 1,
+          10: 1,
+          2: 1,
+        },
+        null,
+        2
+      )
+    );
 
-      done();
-    });
+    const { stdout } = await exec(
+      `node --loader ts-node/esm ./src/index.ts ./src/${testFileName} -d`
+    );
+
+    expect(stdout).toBe(
+      `✨ sort-json-cli: We'd try to sort the following files:\n./src/${testFileName}\n`
+    );
+
+    await fs.promises.rm(path.resolve(__dirname, testFileName));
+  });
+
+  it("should by sort file", async () => {
+    await fs.promises.writeFile(
+      path.resolve(__dirname, testFileName),
+      JSON.stringify(
+        {
+          1: 1,
+          10: 1,
+          2: 1,
+        },
+        null,
+        2
+      )
+    );
+
+    await exec(`node --loader ts-node/esm ./src/index.ts ./src/${testFileName} -r`);
+
+    const filesContentTarget = await fsx.readFile(
+      path.resolve(__dirname, testFileTargetName),
+      "utf8"
+    );
+
+    expect(filesContentTarget).toBe('{\n  "1": 1,\n  "10": 1,\n  "2": 1\n}');
+
+    await fs.promises.rm(path.resolve(__dirname, testFileName));
+    await fs.promises.rm(path.resolve(__dirname, testFileTargetName));
   });
 });
