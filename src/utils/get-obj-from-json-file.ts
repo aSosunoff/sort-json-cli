6882484ca8541eb34b3e6f1f475a122753d8a037
asosunoff <al.sosunoff@gmail.com>
2021-11-25 09:53:29 +0300

    fix

--- src/utils/get-obj-from-json-file.ts
+++ src/utils/get-obj-from-json-file.ts
@@ -3,5 +3,8 @@ import fsx from "fs-extra";
 export const getObjFromJsonFile = async (oneOfPaths: string) => {
   const filesContent = await fsx.readFile(oneOfPaths, "utf8");
 
-  return JSON.parse(filesContent);
+  return {
+    obj: JSON.parse(filesContent) as Record<string, any>,
+    content: filesContent,
+  };
 };
